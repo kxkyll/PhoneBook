a6907274e9262f5a92e9803b366ba67efb259275
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2013-10-09 22:51:34 +0300

    paikkailtu migraatiota

--- db/migrate/20131009154655_remove_person_id_from_addresses.rb
+++ db/migrate/20131009154655_remove_person_id_from_addresses.rb
@@ -1,6 +1,6 @@
 class RemovePersonIdFromAddresses < ActiveRecord::Migration
   def up
-    remove_column :addresses, :person_id, :integer
+    remove_column :addresses, :person_id
   end
 
   def down
