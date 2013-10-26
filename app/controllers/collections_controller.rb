9d5865772844a8d350688baa132b3af2fb8e7d99
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2013-10-26 15:47:53 +0300

    näytetään vain kirjautuneen käyttääjän kokoelmat

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -3,7 +3,7 @@ class CollectionsController < ApplicationController
   # GET /collections.json
   def index
     @collections = Collection.all
-
+   
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @collections }
